5f28fa087407d9b9a29824aad1cdec089772f221
theg <52027622+theg1239@users.noreply.github.com>
2025-03-02 15:01:38 +0530

    chore: some SEO stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,39 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "z0d1ak",
   description: "CTF Writeups and Hacking Adventures",
+  icons: {
+    icon: "/favicon.png",
+  },
+  alternates: {
+    canonical: "https://z0d1ak.vercel.app",
+  },
+  openGraph: {
+    title: "z0d1ak",
+    description: "CTF Writeups and Hacking Adventures",
+    url: "https://z0d1ak.vercel.app",
+    siteName: "z0d1ak",
+    images: [
+      {
+        url: "https://z0d1ak.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "z0d1ak Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "z0d1ak",
+  //   description: "CTF Writeups and Hacking Adventures",
+  //   images: ["https://z0d1ak.com/twitter-image.png"],
+  //   creator: "@yourtwitterhandle",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
